f9f46a6f6f1e885fde55e9a4c3f4584d5ec94477
MScroggs <matthew.w.scroggs@gmail.com>
2016-05-14 21:01:48 +0100

    Added tool to check games for undefined rooms

---
+++ tools/test_game.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import division
+import sys
+sys.path.insert(0,'..')
+from core.ave import Game
+
+game = Game(sys.argv[1],"")
+game.load()
+not_inc = []
+for id in game.rooms:
+    for key in game[id].option_keys:
+        if key not in game.rooms and key != "__GAMEOVER__":
+            not_inc.append(key)
+
+if len(not_inc)>0:
+    print "Rooms not defined:"
+    for key in not_inc:
+        print " ",key
+else:
+    print "Game contains no errors"
