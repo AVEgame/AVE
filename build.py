a40e95a06a8fffd1d5ca9b57cd86c13859fdbf65
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2016-08-12 13:59:22 +0100

    Revert "Resolve conflicts"
    This reverts commit 1a1c6e9c892aaf2321348e52332e6c8ef35bf2d2.

--- build.py
+++ build.py
@@ -11,25 +11,6 @@ class BadArg(BaseException):
     def __str__(self):
         return self.string
 
-class BadFile(BaseException):
-    def __init__(self):
-        pass
-    def __str__(self):
-        return "Game files must end in .ave"
-
-def create_item_text(game):
-    success = False
-    text = ''
-    with open(game, 'r') as f:
-        for line in f:
-            if line[0] == '%':
-                success = True
-            elif line[0] == '#':
-                success = False
-            if success:
-                text += line
-    return text
-
 if len(sys.argv) < 2:
     raise BadArg("Please enter a type to build")
 if sys.argv[1] not in ["python","emf"]:
@@ -51,25 +32,36 @@ for f in os.listdir(b_dir):
         shutil.rmtree(os.path.join(b_dir,f))
 
 # copy games
-os.mkdir(os.path.join(b_dir,"games"))
-for f in os.listdir(os.path.join(dir,"games")):
-    if f[-4:] == ".ave":
-        shutil.copy(os.path.join(os.path.join(dir,"games"),f), os.path.join(os.path.join(b_dir,"games"),f))
-shutil.copy(os.path.join(dir,"VERSION"), os.path.join(b_dir,"VERSION"))
+if sys.argv[1] == "emf":
+    for f in os.listdir(os.path.join(dir,"games")):
+        if f[-4:] == ".ave":
+            shutil.copy(os.path.join(os.path.join(dir,"games"),f), b_dir)
+else:
+    os.mkdir(os.path.join(b_dir,"games"))
+    for f in os.listdir(os.path.join(dir,"games")):
+        if f[-4:] == ".ave":
+            shutil.copy(os.path.join(os.path.join(dir,"games"),f), os.path.join(b_dir,"games"))
 
 # copy version specific files
+shutil.copy(os.path.join(dir,"VERSION"), os.path.join(b_dir,"VERSION"))
+
 if sys.argv[1] == "python":
     shutil.copy(os.path.join(dir,"python/run.py"), b_dir)
     shutil.copytree(os.path.join(dir,"python/core"), os.path.join(b_dir,"core"))
+    shutil.copy(os.path.join(dir,"commoncore/errors.py"), os.path.join(b_dir,"core"))
+    shutil.copy(os.path.join(dir,"commoncore/__init__.py"), os.path.join(b_dir,"core"))
 
 if sys.argv[1] == "emf":
     with open(os.path.join(b_dir,"VERSION"),"a") as f:
         f.write("~")
-    with open(os.path.join(b_dir,"__init__.py"),"w") as f:
-        pass
+    shutil.copy(os.path.join(dir,"commoncore/errors.py"), b_dir)
+    shutil.copy(os.path.join(dir,"commoncore/__init__.py"), b_dir)
     shutil.copy(os.path.join(dir,"emf/main.py"), b_dir)
-    shutil.copytree(os.path.join(dir,"emf/core"), os.path.join(b_dir,"core"))
+    shutil.copy(os.path.join(dir,"emf/core/screen.py"), b_dir)
+    shutil.copy(os.path.join(dir,"emf/core/ave.py"), b_dir)
+    shutil.copy(os.path.join(dir,"emf/core/credits"), b_dir)
+    shutil.copy(os.path.join(dir,"emf/core/title"), b_dir)
+    shutil.copy(os.path.join(dir,"emf/core/screen.py"), b_dir)
+    shutil.copy(os.path.join(dir,"emf/core/utils.py"), b_dir)
 
 # copy common files
-shutil.copy(os.path.join(dir,"commoncore/errors.py"), os.path.join(b_dir,"core"))
-shutil.copy(os.path.join(dir,"commoncore/__init__.py"), os.path.join(b_dir,"core"))
