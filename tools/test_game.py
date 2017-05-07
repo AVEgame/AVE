f7887424ba872a17524951f4dc26b26c39080301
MScroggs <matthew.w.scroggs@gmail.com>
2017-05-07 11:01:37 +0100

    Added not accessible to test

--- tools/test_game.py
+++ tools/test_game.py
@@ -15,14 +15,24 @@ def avefile():
 game = Game(avefile,ds,c)
 game.load()
 not_inc = []
+ach = ["start"]
 for id in game.rooms:
     for key in game[id].options:
+        if key['id'] not in ach:
+            ach.append(key['id'])
         if key['id'] not in game.rooms and key['id'] != "__GAMEOVER__" and key['id'] != "__WINNER__" and key['id'] not in not_inc:
             not_inc.append(key['id'])
 
-if len(not_inc)>0:
-    print "Rooms not defined:"
-    for key in not_inc:
-        print " ",key
+not_ach = [i for i in game.rooms if i not in ach]
+
+if len(not_inc)>0 or len(not_ach)>0:
+    if len(not_inc)>0:
+        print "Rooms not defined:"
+        for key in not_inc:
+            print " ",key
+    if len(not_ach)>0:
+        print "Rooms not accessible:"
+        for key in not_ach:
+            print " ",key
 else:
     print "Game contains no errors"
