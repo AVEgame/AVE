a0d176d2b0fb0d9d4d5084f0215c26d25b16ef33
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-07-01 22:32:05 +0100

    no screen

--- ave/__init__.py
+++ ave/__init__.py
@@ -2,4 +2,3 @@ from .ave import AVE  # noqa: F401
 from .game_loader import load_game_from_library  # noqa: F401
 from .game_loader import load_game_from_file  # noqa: F401
 from .game import Character, Game  # noqa: F401
-from .screen import Screen, DummyScreen  # noqa: F401
